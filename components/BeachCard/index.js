806e85c51d97358e8c525d980b4e288e3b5d9c98
Mikaal <matthewnaik@gmail.com>
2020-06-19 18:12:55 -0400

    Add images to BeachCard

--- components/BeachCard/index.js
+++ components/BeachCard/index.js
@@ -1,17 +1,19 @@
 import styles from './BeachCard.module.scss'
 import Warning from '@/components/Warning/index'
+import moment from 'moment';
 
 const BeachCard = ({beach}) => {
   const { name, eColiCount, beachAdvisory, sampleDate, beachState, beachID } = beach;
   return (
     <div className={styles['beachcard']}>
+      <img src={`/beach-${beachID}.jpg`} className={styles.image}/>
       <Warning beachID={beachID} />
-      <h2>{name}</h2>
-      <b>{eColiCount}</b>
+      <div className={styles.title}>{name}</div>
+      <b>Ecoli: {eColiCount}</b>
       <div>{beachState}</div>
       <div>
         Sample Date:
-          {sampleDate}
+          {moment(sampleDate).format('MMMM Do, YYYY')}
       </div>
     </div>
   )
