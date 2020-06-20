5ff460d1cd6648c0ae6ff8bbaef76ad9813d9abd
Mikaal Naik <mikaalnaik@gmail.com>
2020-06-20 09:56:11 -0400

    Update global style imports

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
-import '../app.scss'
+import '@/styles/app.scss';
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
