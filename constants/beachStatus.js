367732797dc342188e107eb2c698bda8cc65f2c1
Mikaal <matthewnaik@gmail.com>
2020-06-29 19:05:10 -0400

    Update beach status constants after city opened ferry service

--- constants/beachStatus.js
+++ constants/beachStatus.js
@@ -2,15 +2,15 @@ export default function isBeachClosed(beachID) {
   const statuses = {
     1: false,
     2: false,
-    3: true,
-    4: true,
-    5: true,
-    6: true,
+    3: false,
+    4: false,
+    5: false,
+    6: false,
     7: false,
     8: false,
     9: false,
     10: false,
-    11: true,
+    11: false,
   };
   return statuses[beachID];
 }
