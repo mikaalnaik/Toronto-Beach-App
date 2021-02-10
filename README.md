138758dd4bcda29ad1bb8d51c8ef7517129528cf
Mikaal <matthewnaik@gmail.com>
2021-02-10 18:29:20 -0500

    Add weather api and testing static site generation

--- README.md
+++ README.md
@@ -6,64 +6,64 @@
 
 &nbsp;
 
-  
+
 
 The easiest way to view **The City of Toronto's** Open Data beach data.
 
-  
-  
+
+
 
 &nbsp;
 
-  
+
 
 # Getting Started
 
-  
-  
-  
+
+  You'll need an API key from https://www.weatherapi.com/ to access the latest weather info.
+
 
 ### Installation
 
-  
+
 
 &nbsp;
 
 Super easy, first run this:
 
-  
+
 
 `yarn dev`
 
-  
-  
+
+
 
 Then to run:
 
-  
+
 
     yarn dev
 
-  
+
 
 That's it!
 
-  
+
 
 Now start using it with the functions and documentation below.
 
-  
-  
+
+
 
 &nbsp;
 
-  
+
 
 # Contributing
 
-  
-  
-  
+
+
+
 
 ### Please contribute! Create and issue, Pull Requests and we're very excited to have this project open source for the first time
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,33 @@
 import { useState, useEffect } from 'react';
 import Head from 'next/head';
-// const fetch = require('node-fetch');
+import fetch from 'node-fetch';
 import BeachCard from '../components/BeachCard';
 import { BeachPlaceholderData } from '../constants/beachPlaceholderData';
 import styles from './style.module.scss';
 
-export default function Home() {
+
+// This function gets called at build time on server-side.
+// It won't be called on client-side, so you can even do
+// direct database queries. See the "Technical details" section.
+export async function getStaticProps() {
+  const weather = await fetch(`http://api.weatherapi.com/v1/current.json?key=${process.env.WEATHER_API_KEY}&q=toronto`).then(r => r.json());
+
+  const date = new Date().toString();
+
+  return {
+    props: {
+      weather,
+      date,
+    },
+    revalidate: 1000, // In seconds
+  };
+}
+
+export default function Home({ weather, date }) {
+
+  console.log({ date });
   const [beachData] = useState(BeachPlaceholderData);
 
-  useEffect(() => {
-    // fetch('api/getBeachLatest')
-    //   .then(res => res.text())
-    //   .then(data => setBeachData(JSON.parse(data)));
-  }, []);
 
   // const goToBeach = () => {
   //   var win = window.open('https://secure.toronto.ca/FerryTicketOnline/tickets2/index.jsp', '_blank');
@@ -26,13 +41,6 @@ export default function Home() {
         <meta name="description" content="The easiest way to access information about Toronto's 11 beaches and they ferry schedule"></meta>
       </Head>
       <main>
-        {/* <div className={styles.row}>
-          <h1>Toronto Beach Report</h1>
-          <button onClick={goToBeach} className={styles.ferry}>
-            <img src='/ferry.png' />Ferry Tickets
-          </button>
-        </di.v> */}
-
         <span className={styles.row}>
           <h1>
           Toronto's beach water quality
@@ -53,7 +61,7 @@ export default function Home() {
 
         </span>
         <h5>
-          Last updated: February 9th, 2021
+          Last updated: {date}
         </h5>
 
         {/* <h3>Show respect for the health of others and for the beauty of our natural spaces.</h3> */}
