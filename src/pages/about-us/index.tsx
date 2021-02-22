f23dc1652f537b4c8831ea49eef9bcfd4e1c8048
Mikaal <matthewnaik@gmail.com>
2021-02-22 08:41:04 -0500

    Add rudimentary About Us Page

---
+++ src/pages/about-us/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './style.module.scss';
+
+export default function AboutUs() {
+  return (
+    <div className={styles.component}>
+      howdy
+      <div>
+        The Toronto Beach App is an open sourced project focused on improving access to
+      </div>
+    </div>
+  );
+}
