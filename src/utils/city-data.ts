2c10e82ab2a17944ed91c32564a0a6fdf376b1d2
Norman Chan <slowmanchan@gmail.com>
2022-03-11 12:10:13 -0500

    Remove unused imports and pages

--- src/utils/city-data.ts
+++ src/utils/city-data.ts
@@ -1,11 +1,10 @@
-const fetch = require('node-fetch');
-const convert = require('xml-js');
-const _ = require('lodash');
+// const convert = require('xml-js');
+// const _ = require('lodash');
 // const BeachConstants = require('../beachConstants');
 
 export function getAllBeachesAllTime() {
   return getFromApi(
-    'http://app.toronto.ca/tpha/ws/beaches/history/all.xml?v=1.0'
+    // 'http://app.toronto.ca/tpha/ws/beaches/history/all.xml?v=1.0'
   );
 }
 
@@ -13,7 +12,7 @@ export function getAllBeachesLatest() {
   // return getFromApi('http://app.toronto.ca/tpha/ws/beaches.xml?v=1.0');
 }
 
-export function getAllBeachesForRange(start, endDate) {
+export function getAllBeachesForRange() {
   // if (!start || !end) {
   //   throw 'Both a start date and end date are required. We also expect a date format of YYYY-MM-DD';
   // }
@@ -22,7 +21,7 @@ export function getAllBeachesForRange(start, endDate) {
   // );
 }
 
-export function getSpecificBeachForRange(beachID, startDate, endDate) {
+export function getSpecificBeachForRange() {
   // if (!startDate || !endDate || !beachID) {
   //   throw 'Both a start date and end date are required. We also expect a date format of YYYY-MM-DD';
   // }
@@ -31,7 +30,7 @@ export function getSpecificBeachForRange(beachID, startDate, endDate) {
   // );
 }
 
-export function getSpecificBeachAllTime(beachID) {
+export function getSpecificBeachAllTime() {
   // if (!startDate || !endDate || !beachID) {
   //   throw 'Both a start date and end date are required. We also expect a date format of YYYY-MM-DD';
   // }
@@ -40,7 +39,7 @@ export function getSpecificBeachAllTime(beachID) {
   // );
 }
 
-export function getFromApi(url) {
+export function getFromApi() {
   // return fetch(url)
   //   .then((res) => {
   //     return res.text();
@@ -55,35 +54,35 @@ export function getFromApi(url) {
 }
 
 
-export  function formatBeachData(rawData) {
-  const data = JSON.parse(
-    convert.xml2json(rawData, {
-      compact: false,
-      spaces: 4,
-    })
-  );
+// export  function formatBeachData(rawData) {
+//   const data = JSON.parse(
+//     convert.xml2json(rawData, {
+//       compact: false,
+//       spaces: 4,
+//     })
+//   );
 
-  const beaches = _.get(data, ['elements', 0, 'elements', 1, 'elements']);
+//   const beaches = _.get(data, ['elements', 0, 'elements', 1, 'elements']);
 
-  return beaches
-    .map((beach) => {
-      const beachData = _.get(beach, ['elements']);
-      const beachID = _.get(beach, ['attributes', 'beachId']);
-      return {
-        beachID: Number(beachID),
-        name: _.get(BeachConstants, [beachID, 'name']),
-        map: _.get(BeachConstants, [beachID, 'map']),
-        sampleDate: _.get(beachData, [0, 'elements', 0, 'text']),
-        publishDate: _.get(beachData, [1, 'elements', 0, 'text']),
-        eColiCount: Number(_.get(beachData, [2, 'elements', 0, 'text'])),
-        beachAdvisory: _.get(beachData, [3, 'elements', 0, 'text']),
-        beachState: _.get(beachData, [4, 'elements', 0, 'text']),
-      };
-    })
-    .filter((element) => {
-      if (_.get(element, ['name'])) {
-        return element;
-      }
-    });
-};
+//   return beaches
+//     .map(beach => {
+//       const beachData = _.get(beach, ['elements']);
+//       const beachID = _.get(beach, ['attributes', 'beachId']);
+//       return {
+//         beachID: Number(beachID),
+//         name: _.get(BeachConstants, [beachID, 'name']),
+//         map: _.get(BeachConstants, [beachID, 'map']),
+//         sampleDate: _.get(beachData, [0, 'elements', 0, 'text']),
+//         publishDate: _.get(beachData, [1, 'elements', 0, 'text']),
+//         eColiCount: Number(_.get(beachData, [2, 'elements', 0, 'text'])),
+//         beachAdvisory: _.get(beachData, [3, 'elements', 0, 'text']),
+//         beachState: _.get(beachData, [4, 'elements', 0, 'text']),
+//       };
+//     })
+//     .filter(element => {
+//       if (_.get(element, ['name'])) {
+//         return element;
+//       }
+//     });
+// }
 
